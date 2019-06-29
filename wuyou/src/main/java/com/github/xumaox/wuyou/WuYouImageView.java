476c2674f945c230949e22cee410fb8f9735a697
周俊佑 <zhoujunyou@puscene.com>
2019-06-29 21:51:23 +0800

    add wuyou lib 污柚

---
+++ wuyou/src/main/java/com/github/xumaox/wuyou/WuYouImageView.java
@@ -0,0 +1,27 @@
+package com.github.xumaox.wuyou;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+
+/**
+ * Description: for test jitpack
+ *
+ * @author zhou.junyou
+ * Create by:Android Studio
+ * Date:2019-06-29
+ */
+public class WuYouImageView extends AppCompatImageView {
+    public WuYouImageView(Context context) {
+        super(context);
+    }
+
+    public WuYouImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WuYouImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
