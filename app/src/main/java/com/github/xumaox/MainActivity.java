d168628b4ac8152b10a99610ba84fded9e1dc849
xumaolovemiemie <13761719246@163.com>
2019-06-30 09:53:19 +0800

    test

--- app/src/main/java/com/github/xumaox/MainActivity.java
+++ app/src/main/java/com/github/xumaox/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
     }
 }
